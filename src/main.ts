7b236042301d14b71355cbdc23fc935c5b7fd556
Zeke Davidson <zkdavids@ucsc.edu>
2024-11-05 15:25:58 -0800

    Added a button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-// todo
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+const testButton = document.createElement("button");
+testButton.innerHTML = "Test";
+app.append(testButton);
+testButton.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
