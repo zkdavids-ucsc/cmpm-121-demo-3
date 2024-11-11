4afeba19242f2ccd9c5ebf6a54e105f541b9786f
beanieloser <zekes.other.email@gmail.com>
2024-11-10 16:20:12 -0800

    D3.a done i think

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,121 @@
+// @deno-types="npm:@types/leaflet@^1.9.14"
+import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css";
 import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+// Fix missing marker images
+import "./leafletWorkaround.ts";
 
-const testButton = document.createElement("button");
-testButton.innerHTML = "Test";
-app.append(testButton);
-testButton.addEventListener("click", () => {
-  alert("You clicked the button!");
+// Deterministic random number generator
+import luck from "./luck.ts";
+
+// Location of our classroom (as identified on Google Maps)
+const LOCATION = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+// Tunable gameplay parameters
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+// Create the map (element with id "map" is defined in index.html)
+const map = leaflet.map(document.getElementById("map")!, {
+  center: LOCATION,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
+
+// Populate the map with a background tile layer
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+// Add a marker to represent the player
+const playerMarker = leaflet.marker(LOCATION);
+playerMarker.bindTooltip("That's you!");
+playerMarker.addTo(map);
+
+// Display the player's points
+let playerPoints = 0;
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!; // element `statusPanel` is defined in index.html
+statusPanel.innerHTML = "No points yet...";
+
+// Add caches to the map by cell numbers
+function spawnCache(i: number, j: number) {
+  // Convert cell numbers into lat/lng bounds
+  const origin = LOCATION;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  // Add a rectangle to the map to represent the cache
+  const cache = leaflet.rectangle(bounds);
+  cache.addTo(map);
+
+  // Handle interactions with the cache
+  cache.bindPopup(() => {
+    // Each cache has a random point value, mutable by the player
+    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+
+    // The popup offers a description and button
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+                <div>There is a cache here at "${i},${j}". It has value <span id="value">${pointValue}</span>.</div>
+                <button id="collect">collect</button><button id="deposit">deposit</button>`;
+
+    // Clicking the button decrements the cache's value and increments the player's points
+    popupDiv
+      .querySelector<HTMLButtonElement>("#collect")!
+      .addEventListener("click", () => {
+        if (pointValue > 0) {
+          pointValue--;
+          popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+            pointValue.toString();
+          playerPoints++;
+          updatePoints();
+        } else {
+          statusPanel.innerHTML = `No more points to collect!`;
+        }
+      });
+
+    popupDiv
+      .querySelector<HTMLButtonElement>("#deposit")!
+      .addEventListener("click", () => {
+        if (playerPoints > 0) {
+          pointValue++;
+          popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+            pointValue.toString();
+          playerPoints--;
+          updatePoints();
+        } else {
+          statusPanel.innerHTML = `Not enough points to deposit :(`;
+        }
+      });
+
+    return popupDiv;
+  });
+}
+
+function updatePoints() {
+  statusPanel.innerHTML = `You have ${playerPoints} points`;
+}
+
+// Look around the player's neighborhood for caches to spawn
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    // If location i,j is lucky enough, spawn a cache!
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(i, j);
+    }
+  }
+}
